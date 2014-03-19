0efeeb6f7e7b31c0a0c6e4ee9adc0502cb9df578
Rygaido <rygaido@gmail.com>
2014-03-19 07:30:44 -0400

    inputWindow&obstacle
    add the missing class stubs

---
+++ Hax/Hax/InputWindow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//takes player input string
+namespace Hax {
+    class InputWindow {
+        string input;
+    }
+}
